2e84f59b43a9b7fbff4277d617250a7e86090a5d
Sultan Saadat <sultansaadat@gmail.com>
2014-06-12 14:33:13 +0500

    refactor

--- app.js
+++ app.js
@@ -13,62 +13,24 @@ var users = require('./routes/users');
 var student = require('./routes/student');
 var courses = require('./routes/courses');
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-cons = require('consolidate'),
+var cons = require('consolidate');
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.get('/', function(req, res) {
-    res.render("index");
-});
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
-app.use('/', routes);
-app.use('/users', users);
 app.get('/student', student.showStudents);
 app.post('/student', student.addStudents);
 app.get('/courses', courses.showCourses);
 app.post('/courses', courses.addCourses)
 
-
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-
-
-
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
 app.listen(4000, function() {
     console.log('message:"Server is live and Ready" ')
 });
